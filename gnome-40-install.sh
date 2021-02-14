262971db0671d02aee2c63900fdabc9c0d28d51f
Himansh <himansh02@hotmail.com>
2021-02-14 23:00:44 +0530

    Update gnome-40-install.sh

--- gnome-40-install.sh
+++ gnome-40-install.sh
@@ -8,23 +8,34 @@
 
 
 # Setting up prereqs
-#//// sudo pacman -S gnome
+# installing the complete gnome lib is the best way to satisfy few dependencies
+sudo pacman -S gnome # You can always unisntall late
+
 sudo pacman -S cmake
 sudo pacman -S gn-m85
-sudo pacman -S glib2-git
+sudo pacman -S glib2-git # Looks like there is some problem in the prepare(), wait for it to get fixed
 sudo pacman -S sassc
 sudo pacman -S asciidoc
+sudo pacman -S yay
+
+
+yay -S gnome-shell-40alfa
+yay -S mutter-40alfa
+
+# It should be done by now
 
 
 
+
+# this is for updating to the latest version
 # Setting up workspace
 mkdir GnomeShell40
 cd GnomeShell40
 
 
 # Gathering repos
-git clone https://gitlab.gnome.org/GNOME/mutter
-git clone https://gitlab.gnome.org/feaneron/gnome-shell/
+git clone https://gitlab.gnome.org/GNOME/mutter &
+git clone https://gitlab.gnome.org/feaneron/gnome-shell/ &
 
 
 # Installing mutter
@@ -32,6 +43,7 @@ cd  mutter
 meson _build --prefix=/usr
 sudo ninja install -C _build
 
+cd..
 
 # Installing gnome-shell-40
 cd gnome-shell
